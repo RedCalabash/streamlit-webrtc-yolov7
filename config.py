b7127252620cf953f32ebc19613c525e051d5146
RedCalabash <75779246+RedCalabash@users.noreply.github.com>
2023-02-01 10:19:23 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+
+HERE = Path(__file__).parent
+
+MODEL_PATH = HERE / "./models/"
+
+# https://github.com/PINTO0309/PINTO_model_zoo
+MODEL_URL = "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/307_YOLOv7/no-postprocess/resources.tar.gz"
+
+# This is only needed when hosting in local
+# Download from: https://www.gyan.dev/ffmpeg/builds/
+FFMPEG_PATH = HERE / "./ffmpeg/ffmpeg.exe"
+
+DEBUG = False
+
+STYLES = {
+    "yolov7-tiny_256x320": "yolov7-tiny_256x320",
+    "yolov7-tiny_256x480": "yolov7-tiny_256x480",
+    "yolov7-tiny_256x640": "yolov7-tiny_256x640",
+    "yolov7-tiny_384x640": "yolov7-tiny_384x640",
+    "yolov7-tiny_480x640": "yolov7-tiny_480x640",
+    "yolov7-tiny_640x640": "yolov7-tiny_640x640",
+    "yolov7-tiny_736x1280": "yolov7-tiny_736x1280",
+}
+
